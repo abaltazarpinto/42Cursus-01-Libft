a12c9f740d06edff1a5f5d7e07d7ea8897c9fc45
Andre Baltazar Pinto <abaltaza@c1r8s10.42porto.com>
2022-11-11 17:11:24 +0000

    ads

---
+++ ft_substr.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaltaza <abaltaza@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/11 17:09:09 by abaltaza          #+#    #+#             */
+/*   Updated: 2022/11/11 17:11:24 by abaltaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char     *ft_substr(char const *s, unsigned int start, size_t len)
+{
+    unsigned int    i;
+    unsigned int    j;
+    char                *str;
+
+    i = start;
+
+}
