d4a3fed2cd08ce7fc6c3b4c59aac52cf0a5d25d1
André Pinto <apinto@mi-gso.com>
2022-11-15 11:24:52 +0000

    atoi

---
+++ ft_atoi.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaltaza <abaltaza@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 11:12:10 by abaltaza          #+#    #+#             */
+/*   Updated: 2022/11/15 11:12:10 by abaltaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	sign;
+	int	n;
+
+	i = 0;
+	n = 0;
+	sign = 1;
+	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == 45 || str[i] == 43)
+	{
+		if (str[i] == '-')
+		{
+			sign = -1;
+		}
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		n = n * 10 + (str[i] - 48);
+		i++;
+	}
+	return (sign * n);
+}
+
+/*int	main(void)
+{
+	char    str[] = "   -----1234ab567";
+        printf("String: %s\n", str);
+        printf("Return: %d", ft_atoi(str));
+}*/
