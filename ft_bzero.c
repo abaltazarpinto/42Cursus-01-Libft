fe9e375ce102ff8e25abdec3b11ac834dfd19d6d
André Pinto <apinto@mi-gso.com>
2022-11-15 12:02:14 +0000

    calloc

---
+++ ft_bzero.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaltaza <abaltaza@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 11:47:27 by abaltaza          #+#    #+#             */
+/*   Updated: 2022/11/15 11:47:27 by abaltaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+SYNOPSIS:
+#include <strings.h>
+
+DESCRIPTION:
+    The bzero() function erases the data in th n bytes of the memory starting at the location pointed to by s, by writing zeros (bytes containing '\0') to that area.
+*/
+
+#include "libft.h"
+
+void	ft_bzero(void *str, size_t n)
+{
+	unsigned char	*p;
+
+	p = str;
+	n++;
+	while (--n)
+	{
+		*p++ = 0;
+	}
+}
+
+/*int	main(void)
+{
+	char	arr[8] = "ABCDefgh";
+	printf("String: %s\n", arr);
+	ft_bzero(arr, 2);
+	printf("Return: %s", arr);
+	return (0);
+}*/
---
+++ ft_calloc.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaltaza <abaltaza@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/15 11:54:09 by abaltaza          #+#    #+#             */
+/*   Updated: 2022/11/15 11:54:09 by abaltaza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Library: #include <stdio.h>
+
+DESCRIPTION:
+What is Calloc in C?
+    The calloc() function in C is used to allocate a specified amount of memory and then initialize it to zero. 
+    The function returns a void pointer to this memory location, which can then be cast to the desired type. THe
+    function takis in two parameters that collectively specify the amount of memory to be allocated. 
+
+*/
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nitems, size_t size)
+{
+	char			*array;
+	size_t			i;
+	unsigned int	total;
+
+	i = 0;
+	total = nitems * size;
+	array = malloc(nitems * size);
+	if (arr == NULL)
+		return (NULL);
+	while (total > 0)
+	{
+		array[i] = 0;
+		total--;
+		i++;
+	}
+	return ((void *)array);
+}
+
+/*int	main(void)
+{
+	printf("Return: %s", (char *)ft_calloc(5, 4));
+}*/
