a397cca0fdc2c54383de62df083880fd1a4bdbff
Andre Baltazar Pinto <abaltaza@c1r8s10.42porto.com>
2022-12-13 20:26:11 +0000

    falta pouco

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abaltaza <abaltaza@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: abaltaza <abaltaza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 11:12:10 by abaltaza          #+#    #+#             */
-/*   Updated: 2022/11/15 11:12:10 by abaltaza         ###   ########.fr       */
+/*   Updated: 2022/12/13 20:24:08 by abaltaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,10 @@ int	ft_atoi(char *str)
 	i = 0;
 	n = 0;
 	sign = 1;
+
 	while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
 		i++;
+
 	if (str[i] == 45 || str[i] == 43)
 	{
 		if (str[i] == '-')
@@ -31,6 +33,7 @@ int	ft_atoi(char *str)
 		}
 		i++;
 	}
+	
 	while (str[i] >= 48 && str[i] <= 57)
 	{
 		n = n * 10 + (str[i] - 48);
